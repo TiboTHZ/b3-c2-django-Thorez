7b413d3b180f8cf031c2cd9681b41c5db872898d
Tibo <t.thorez59@gmail.com>
2023-12-30 11:53:51 +0100

    Add : views & create forms

---
+++ gestion_password/foms.py
@@ -0,0 +1,8 @@
+# gestion_sites/forms.py
+from django import forms
+from .models import Site
+
+class SiteForm(forms.ModelForm):
+    class Meta:
+        model = Site
+        fields = ['nom', 'url', 'identifiant', 'mot_de_passe']
