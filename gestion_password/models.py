6cb18a940b8a2560dccb71a24eebdf5af5545557
Tibo <t.thorez59@gmail.com>
2023-12-30 11:42:10 +0100

    Add: models

--- gestion_password/models.py
+++ gestion_password/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Site(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    nom = models.CharField(max_length=255)
+    url = models.URLField()
+    identifiant = models.CharField(max_length=255)
+    mot_de_passe = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nom
