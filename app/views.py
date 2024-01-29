6ca54f2d4b086c267c2ff66e8d81f7a55bfb6c1b
Tibo <t.thorez59@gmail.com>
2024-01-29 14:44:41 +0100

    Update: Display in web

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Site
 
-# Create your views here.
+def liste_sites(request):
+    sites = Site.objects.all()
+    return render(request, 'sites/liste_sites.html', {'sites': sites})
