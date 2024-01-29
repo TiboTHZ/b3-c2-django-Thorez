0d1e5304d061fea0d8ca466e0c9bdbb64b55cec8
Tibo <t.thorez59@gmail.com>
2024-01-29 14:17:23 +0100

    Update: Migration model

--- password_manager/settings.py
+++ password_manager/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'password_manager_app.apps.PasswordManagerAppConfig',
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
