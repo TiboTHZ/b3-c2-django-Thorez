c34f389ffbfe80c7404b3ac253f0063431acff02
Tibo <t.thorez59@gmail.com>
2024-01-29 14:13:41 +0100

    Add: app setting

--- password_manager/settings.py
+++ password_manager/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'password_manager_app.apps.PasswordManagerAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
